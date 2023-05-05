ccd6196dfc17a175c5915d44590bfc61ddd731c9
David Magazine <67346625+DJMStack@users.noreply.github.com>
2023-05-04 22:48:37 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import random
+
+
+print("Welcome to Rock Paper Scissor games")
+
+playername = input("Enter your name player 1 : ")
+
+score = 0
+
+actions = ['rock', 'paper', 'scissors']
+cpu = random.choice(actions)
+
+player1 = input("Choose rock, paper, or scissors: \n")
+
+
+
+
+
+###scoreboard
+playerdata = player1 + score
+
+leaderboard = [score]
+leaderboard.sort()
+playerdata.append()
+
